79068193c3571a6f314f21adba72cc5fe1136503
GiuseppeSoraci <78480251+GiuseppeSoraci@users.noreply.github.com>
2021-04-06 17:43:04 +0200

    difficulty level

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-/*
-- Il computer deve generare 16 numeri casuali da 1 a 100 (bombe).
-- In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta, se il numero è presente nella lista dei numeri generati la partita termina altrimenti continua chiedendo all’utente un altro numero.
-- La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-- Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-BONUS: (da fare solo se funziona tutto il resto)
-all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-con difficoltà 0 => tra 1 e 100
-con difficoltà 1 => tra 1 e 80
-con difficoltà 2 => tra 1 e 50
-*/
-
 // Setup
-var maxNumber = 10;
-var bombsNumber = 2;
+var maxNumber;
+var bombsNumber = 16;
 var possibilities = maxNumber - bombsNumber;
 var bombList = [];
 var allowedNumbers = [];
 var user = 0;
+
+// Choose the difficulty level
+var level = parseInt(prompt("Choose the difficulty level from 0 to 2").trim());
+// Validation 
+while(isNaN(level) || level < 0 || level > 2) {
+    level = parseInt(prompt("Sorry, the difficulty level you entered doesn't exist yet. \nChoose the difficulty level from 0 to 2").trim());
+}
+
+// Check level
+switch(level) {
+    case 0:
+        maxNumber = 100;
+        break;
+    case 1:
+        maxNumber = 80;
+        break;
+    case 2:
+        maxNumber = 50;
+}
+
+possibilities = maxNumber - bombsNumber;
 
 // Bombs Generation 
 while (bombList.length < bombsNumber) {
@@ -37,7 +46,7 @@ while ((allowedNumbers.length < possibilities) && (!bombList.includes(user))) {
         user = parseInt(prompt("Sorry, value is invalid. \nPlease enter a number from 1 to " + maxNumber));
     }
 
-    // Check 
+    // Check choice
     if (bombList.includes(user)) {
         alert("What a pity! You lost. \nYou successfully tried " + allowedNumbers.length + " times before you found the bomb.")
     } else if (allowedNumbers.includes(user)) {
